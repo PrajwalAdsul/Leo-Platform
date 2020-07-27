b32e55a4e0c2b7f91cb987177d91967171aaec5b
mayank_jain <mayank20399@gmail.com>
2020-07-27 23:41:16 +0530

    Removed scraping code

--- LeoMine/scraper/server.py
+++ LeoMine/scraper/server.py
@@ -1,29 +1,16 @@
-import csv
 import datetime
 import json
-import random
-import string
-import threading
-import time
-from datetime import datetime
 
-import pandas as pd
 import pymongo
-import pytz
-import requests
 from bson import ObjectId, json_util
 from bson.son import SON
-from flask import Flask, jsonify, make_response, request, send_file, send_from_directory
+from flask import Flask, request
 from flask_cors import CORS
-from flask_ngrok import run_with_ngrok
-
-from LeoMine_NewSources import LeoMineScraper
 
 
 class MongoDbEncoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, datetime):
-            print("check")
+        if isinstance(obj, (datetime.datetime, datetime.date)):
             return obj.strftime("%d %B %Y, %I:%M %p")
         if isinstance(obj, ObjectId):
             return str(obj)
@@ -32,122 +19,6 @@ class MongoDbEncoder(json.JSONEncoder):
 
 app = Flask(__name__)
 CORS(app)
-
-
-@app.before_first_request
-def activate_job():
-    def scrape_news():
-        while True:
-            # Scrape for news and calculate crime stats
-            print("Scraping news and calculating crime stats...")
-            LeoMineScraper()
-            print("Scraping news and calculating crime stats done!")
-
-            # Read news stored in default file
-            print("Storing news into the database...")
-            news = pd.read_csv(
-                "./database/headlines.csv",
-                usecols=[
-                    "headline",
-                    "url",
-                    "crime",
-                    "location",
-                    "region",
-                    "city",
-                    "date",
-                ],
-            )
-            news["date"] = news["date"].fillna(datetime.now())
-            # Convert news to json to allow to store in mongodb
-            json_news = news.to_dict(orient="records")
-            # Finally insert the data into mongo
-            result = db.news.insert_many(json_news)
-            print("Number of news saved into database:", len(result.inserted_ids))
-
-            # Read crime stats stored in default file
-            print("Storing crimes into the database...")
-            with open("./database/data.json") as handle:
-                # Load data from JSON to dict
-                file_data = json.load(handle)
-                # Iterate over list of crime objects
-                results = []
-                for data in file_data:
-                    # Fetch approximate coordinates of region
-                    location_details = requests.get(
-                        "https://api.opencagedata.com/geocode/v1/json?q="
-                        + data["Regions"].strip()
-                        + "&key=4a4590286e2c474ca287e179cd718be9"
-                    ).json()
-
-                    # Try removing white spaces and retry
-                    if len(location_details["results"]) == 0:
-                        location_details = requests.get(
-                            "https://api.opencagedata.com/geocode/v1/json?q="
-                            + data["Regions"].replace(" ", "")
-                            + "&key=4a4590286e2c474ca287e179cd718be9"
-                        ).json()
-                        if len(location_details["results"]) == 0:
-                            latitude, longitude = 0.0, 0.0
-                        else:
-                            latitude, longitude = location_details["results"][0][
-                                "geometry"
-                            ].values()
-                    else:
-                        latitude, longitude = location_details["results"][0][
-                            "geometry"
-                        ].values()
-
-                    # Make an UPSERT query
-                    result = db.crimes.update_one(
-                        {"region": data["Regions"]},
-                        {
-                            "$set": {
-                                "region": data["Regions"],
-                                "city": data["City"],
-                                "loc": {
-                                    "type": "Point",
-                                    "coordinates": [longitude, latitude],
-                                },
-                                "murder": data["Murder"],
-                                "rape": data["Rape"],
-                                "kidnapping": data["Kidnapping"],
-                                "robbery": data["Robbery"],
-                                "holding hostage": data["Holding hostage"],
-                                "riot": data["Riot"],
-                                "arson": data["Arson"],
-                                "assault": data["Assault"],
-                                "covid": data["Covid"],
-                            }
-                        },
-                        upsert=True,
-                    )
-                    results.append(result.upserted_id)
-                print("Number of crimes upserted into database:", len(results))
-
-    thread = threading.Thread(target=scrape_news)
-    thread.setDaemon(True)
-    thread.start()
-
-
-def start_runner():
-    def start_loop():
-        not_started = True
-        while not_started:
-            print("Checking if server has started yet...")
-            try:
-                r = requests.get("http://127.0.0.1:5000/")
-                if r.status_code == 200:
-                    print("Server started!")
-                    not_started = False
-                print(r.status_code)
-            except:
-                print("Server has not started yet!")
-            time.sleep(2)
-
-    print("Started runner to activate job : scraping news and caluculating crime stats")
-    thread = threading.Thread(target=start_loop)
-    thread.setDaemon(True)
-    thread.start()
 
 
 @app.route("/")
@@ -186,7 +57,7 @@ def news_by_city(city):
         cursor = db.news.find(
             {"$text": {"$search": city}},
             {"headline": 1, "url": 1, "crime": 1, "city": 1, "date": 1},
-        )
+        ).sort([("date", -1)])
         docs_list = list(cursor)
         return json.dumps(docs_list, cls=MongoDbEncoder)
 
@@ -252,10 +123,6 @@ if __name__ == "__main__":
     )
     db = client.get_database("Leo")
 
-    # Drop existing collections
-    """ db.news.drop()
-    db.crimes.drop() """
-
     # Create index on city in news collection
     db.news.create_index(
         [("city", pymongo.TEXT)], name="search_index", default_language="english"
@@ -263,9 +130,6 @@ if __name__ == "__main__":
 
     # Create index on loc in crimes collection
     db.crimes.create_index([("loc", pymongo.GEOSPHERE)], name="geosearch_index")
-
-    # Start another thread for job
-    start_runner()
 
     # Start flask server
     app.run(port=5000, debug=True)
